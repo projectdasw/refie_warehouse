10c13a375d6fcc0958340777d68d9ebd37336b48
DASW Project <project.dasw@gmail.com>
2024-09-06 12:50:35 +0700

    Update style dan front-end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,62 @@
+// SERVER SIDE PROCESSING
+$('#datamaster_aktivitas').DataTable({
+    serverSide: true,
+    processing: true,
+    ajax: {
+        url: 'inc/datamaster_aktivitas.php',
+        type: 'post'
+    },
+    order: [[0, 'desc']],
+    columnDefs: [
+        {
+            target: 0,
+            visible: false,
+        },
+    ]
+});
+
+$('#datamaster_barangmasuk').DataTable({
+    serverSide: true,
+    processing: true,
+    ajax: {
+        url: 'inc/datamaster_barangmasuk.php',
+        type: 'post'
+    },
+    order: [[0, 'desc']],
+    columnDefs: [
+        {
+            target: 0,
+            visible: false,
+        },
+    ]
+});
+
+$('#datamaster_barangkeluar').DataTable({
+    serverSide: true,
+    processing: true,
+    ajax: {
+        url: 'inc/datamaster_barangkeluar.php',
+        type: 'post'
+    },
+    order: [[0, 'desc']],
+    columnDefs: [
+        {
+            target: 0,
+            visible: false,
+        },
+    ]
+});
+
+$('#datamaster_barang').DataTable({
+    serverSide: true,
+    processing: true,
+    ajax: {
+        url: 'inc/datamaster_barang.php',
+        type: 'post'
+    },
+    order: false,
+});
+
 $(document).ready( function () {
     $('#table-barang').DataTable();
     $('#table-barang-masuk').DataTable({
@@ -30,6 +89,52 @@ $(document).ready( function () {
     });
 });
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+$( '#nama_barang_select' ).select2( {
+    theme: 'bootstrap-5',
+    allowClear: true
+} );
+
+$( '#kategori_select' ).select2( {
+    theme: 'bootstrap-5',
+    allowClear: true
+} );
+
+$( '#kondisi_select' ).select2( {
+    theme: 'bootstrap-5',
+    allowClear: true
+} );
+
+$( '#level_edit' ).select2( {
+    theme: 'bootstrap-5',
+    allowClear: true,
+} );
+
+$( '#level_add' ).select2( {
+    theme: 'bootstrap-5',
+    allowClear: true,
+    dropdownParent: $('#modal_tambahakun')
+} );
+
 setTimeout(() => {
     var alert = document.getElementById('alert-animation');
     alert.style.display = "none";
@@ -53,12 +158,10 @@ function checkFileSelection() {
     var showhideFoto = document.getElementById('showhideFoto');
 
     if (fileInput.files.length > 0) {
-        statusFoto.innerHTML = "<i class='fa-solid fa-check'></i>Foto telah terpilih";
+        statusFoto.innerHTML =
+            "<i class='fa-solid fa-check text-light me-2 text-light'></i><span class='text-light'>Foto telah terpilih</span>";
         statusFoto.style.backgroundColor = "#304D30";
         showhideFoto.style.display = "flex";
-    } else {
-        statusFoto.innerHTML = "Pilih Foto";
-        showhideFoto.style.display = "none";
     }
 }
 
@@ -68,7 +171,7 @@ function hapusFoto() {
     var showhideFoto = document.getElementById('showhideFoto');
 
     fileInput.value = null;
-    statusFoto.innerHTML = "Foto telah dihapus";
+    statusFoto.innerHTML = "Pilihan Foto telah dihapus";
     statusFoto.removeAttribute("style")
     showhideFoto.style.display = "none";
 }
@@ -79,12 +182,11 @@ function formatUang(input){
     input.value = value;
 }
 
-function ambil_detail_masuk(){
+function ambil_detail(){
     var id_barang = document.getElementById("id_barang");
     var nama_barang = document.getElementById("nama_barang");
     var kategori = document.getElementById("kategori");
-    var kondisi = document.getElementById("kondisi");
-    var harga = document.getElementById("harga_jual");
+    var harga = document.getElementById("harga");
 
     var ajax = new XMLHttpRequest();
     ajax.open("POST", "inc/onchange.php", true);
@@ -99,45 +201,17 @@ function ambil_detail_masuk(){
                 var id = data[a].id_barang;
                 var nm = data[a].nama_barang;
                 var cat = data[a].kategori;
-                var knd = data[a].kondisi;
                 var hrg = data[a].harga;
 
                 if(nama_barang.value === nm){
                     id_barang.value = id;
                     kategori.value = cat;
-                    kondisi.value = knd;
                     harga.value = hrg;
                 }
-            }
-        }
-    }
-}
-
-function ambil_detail_keluar(){
-    var id_barang = document.getElementById("id_barang");
-    var nama_barang = document.getElementById("nama_barang");
-    var kategori = document.getElementById("kategori");
-    var harga = document.getElementById("harga_jual");
-
-    var ajax = new XMLHttpRequest();
-    ajax.open("POST", "inc/onchange.php", true);
-    ajax.send();
-
-    ajax.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            var data = JSON.parse(this.responseText);
-            console.log(data);
-
-            for(var a = 0; a < data.length; a++){
-                var id = data[a].id_barang;
-                var nm = data[a].nama_barang;
-                var cat = data[a].kategori;
-                var hrg = data[a].harga;
-
-                if(nama_barang.value === nm){
-                    id_barang.value = id;
-                    kategori.value = cat;
-                    harga.value = hrg;
+                else{
+                    id_barang.value = null;
+                    kategori.value = null;
+                    harga.value = null;
                 }
             }
         }
